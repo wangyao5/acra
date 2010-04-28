b6d040b3079106bbacdd56e2e576022aa36c3d99
kevin.gaudin <kevin.gaudin@8844f115-f6c1-79da-096e-1e6bcc7590e6>
2010-04-28 23:09:23 +0000

    - Defined socket connection/read timeouts instead of default values - Execute report sending on application start in a different thread. This prevents UI lags when sending several reports - Delete report files after sending them. Fixes issue : we were losing reports

---
+++ tags/REL-1_2_0/CrashReport/sample/org/acra/sampleapp/CrashTestActivity.java
@@ -0,0 +1,18 @@
+package org.acra.sampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrashTestActivity extends Activity {
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @SuppressWarnings("null")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Object nul = null;
+        nul.toString();
+    }
+}
