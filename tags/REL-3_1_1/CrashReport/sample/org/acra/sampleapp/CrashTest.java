611ce6a403bdc625b97333fdf56d36fbb3b5994e
kevin.gaudin@gmail.com <kevin.gaudin@gmail.com@8844f115-f6c1-79da-096e-1e6bcc7590e6>
2011-02-26 15:59:42 +0000

    Added release 3.1.1

---
+++ tags/REL-3_1_1/CrashReport/sample/org/acra/sampleapp/CrashTest.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2010 Emmanuel Astier & Kevin Gaudin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.acra.sampleapp;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import android.app.Application;
+
+@ReportsCrashes(formKey = "dEhXSldocldoUDBzR25jM2dtSGo2QXc6MQ",
+        mode = ReportingInteractionMode.NOTIFICATION,
+        resToastText = R.string.crash_toast_text,
+        resNotifTickerText = R.string.crash_notif_ticker_text,
+        resNotifTitle = R.string.crash_notif_title,
+        resNotifText = R.string.crash_notif_text,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt)
+public class CrashTest extends Application {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Application#onCreate()
+     */
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+    }
+
+}
